3772f51d2fc4ad7a8964ee14e55c190a7d1af709
sravansai-26 <sai1234comon@gmail.com>
2025-12-09 21:02:58 +0530

    Changing handset

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lyfspot.resolveit',
+  appName: 'Resolveit',
+  webDir: 'client/dist'
+};
+
+export default config;
